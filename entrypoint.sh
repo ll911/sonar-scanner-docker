6449d6f0d9c53c1d9bf1e423d031048b9bf55479
Leo <leo.lou@gov.bc.ca>
2018-07-04 14:55:12 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [[ "$1" == "sonarScan" ]]; then
+if [[ "$1" == "sonar-scanner" ]]; then
   git clone $gitRepo /tmp/src
 
   if [[ "$2" == "run" || "$2" == "scan" ]]; then
@@ -9,7 +9,8 @@ if [[ "$1" == "sonarScan" ]]; then
     export PATH=$SS_HOME/bin:$SS_HOME/jre/bin:$PATH 
     export LD_LIBRARY_PATH=$SS_HOME/lib:$SS_HOME/jre/lib:$LD_LIBRARY_PATH
     
-    sonar-scanner -Dsonar.login=$sonarToken -Dsonar.projectKey=$repoName -Dsonar.projectVersion=$gitTag
+    sonar-scanner -Dsonar.login=$sonarToken -Dsonar.host.url=$sonarqubeSvc \
+    -Dsonar.projectKey=$repoName -Dsonar.projectVersion=$gitTag -Dsonar.sources=/tmp/src 
   fi
 fi
 exec "$@"
